64b3d1d1a239df9e1fee18138b53050b2030d186
rmkofficial <kmert0@outlook.com>
2024-04-18 13:37:47 +0300

    Add search functionality to Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [searchText, setSearchText] = useState("");
+
+  const handleInputChange = (event) => {
+    let inputText = event.target.value;
+
+    if (inputText.length > 0) {
+      inputText = inputText.charAt(0).toUpperCase() + inputText.slice(1);
+    }
+
+    setSearchText(inputText);
+  };
+
+  const handleSearch = () => {
+    console.log("Search Text:", searchText);
+  };
+
   return (
     <>
       <h1>Home</h1>
-      <h2>
-        <Link to="/weather">Weather</Link>
-      </h2>
+      <input
+        type="text"
+        placeholder="Enter your search text"
+        value={searchText}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleSearch}>
+        <Link to="/weather">Search</Link>
+      </button>
     </>
   );
 };
