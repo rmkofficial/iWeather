36f96480b2dc79eddbe352052516eb269b578e65
rmkofficial <kmert0@outlook.com>
2024-04-20 23:01:54 +0300

    Update Home.css and Home.jsx to add popover section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -86,7 +86,7 @@ const Home = () => {
                       key={suggestion.id}
                       onClick={() => handleSelectCity(suggestion)}
                     >
-                      {suggestion.name}, {suggestion.region} -{" "}
+                      {suggestion.name} -{" "}
                       {suggestion.country}
                     </li>
                   ))}
