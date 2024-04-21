25d840fdc91b881e9e6a8591b39ee52132463e7d
rmkofficial <kmert0@outlook.com>
2024-04-21 23:32:55 +0300

    Refactor Weather component to import and display weather icons and improve code readability

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,14 @@
 import "./Weather.css";
+import Thermal from "./../../public/images/thermal.svg";
+import Rain from "./../../public/images/rain.svg";
+import Wind from "./../../public/images/wind.svg";
+import Humidity from "./../../public/images/air-humidity.svg";
+import Uv from "./../../public/images/uv.svg";
+// import Storm from "../../public/images/storm-day.png";
+// import Rainday from "../../public/images/rain-day.png";
+// import Cloudy from "../../public/images/cloudy-day.png";
+// import FewClouds from "../../public/images/fewClouds.png";
+// import Clear from "../../public/images/clear-day.png";
 import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
@@ -14,6 +24,11 @@ const Weather = ({ city }) => {
       day: "numeric",
       year: "numeric",
     });
+  };
+
+  const formatDayOfWeek = (timestamp) => {
+    const date = new Date(timestamp * 1000);
+    return date.toLocaleString("en-US", { weekday: "long" });
   };
 
   const kelvinToCelsius = (kelvin) => {
@@ -87,45 +102,45 @@ const Weather = ({ city }) => {
           {/* Additional Weather Details Section */}
           <div className="detail-section">
             <p>
+              <img src={Thermal} alt="" />
               Thermal Sensation:{" "}
               <span>
                 {kelvinToCelsius(weatherData.current.main.feels_like)} °C
               </span>
             </p>
             <p>
+              <img src={Rain} alt="" />
               Probability of Rain:
               <span>{weatherData.current.main.humidity}%</span>{" "}
             </p>
             <p>
+              <img src={Wind} alt="" />
               Wind Speed:
               <span>{weatherData.current.wind.speed} m/s</span>
             </p>
             <p>
+              <img src={Humidity} alt="" />
               Air Humidity: <span>{weatherData.current.main.humidity}%</span>
             </p>
             <p>
+              <img src={Uv} alt="" />
               Cloudiness: <span>{weatherData.current.clouds.all}%</span>
             </p>
           </div>
 
           {/* Forecast Weather Section */}
           <div className="forecast-section">
-            <h3>Forecast Weather</h3>
             {weatherData.forecast.list.map((forecastItem) => (
               <div key={forecastItem.dt}>
-                <p>{formatDate(forecastItem.dt)}</p>
-                <p>
-                  Max Temperature: {kelvinToCelsius(forecastItem.main.temp_max)}{" "}
-                  °C
-                </p>
-                <p>
-                  Min Temperature: {kelvinToCelsius(forecastItem.main.temp_min)}{" "}
-                  °C
-                </p>
+                <p>{formatDayOfWeek(forecastItem.dt)}</p>
                 <img
                   src={`http://openweathermap.org/img/wn/${forecastItem.weather[0].icon}.png`}
                   alt={forecastItem.weather[0].description}
                 />
+                <div className="list">
+                  <p>{kelvinToCelsius(forecastItem.main.temp_max)}°C</p>
+                  <p>{kelvinToCelsius(forecastItem.main.temp_min)}°C</p>
+                </div>
               </div>
             ))}
           </div>
